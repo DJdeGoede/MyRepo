82a23bdb2773edec420e3b5fb9535d7eca9cff53
Dick de Goede <djdegoed@gmail.com>
2021-05-15 10:44:05 +0200

    Updates

--- arguments.sh
+++ arguments.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-echo "The $0 script got the argument $1"
+while getopts :u:p:a:b option; do
+	case $option in
+		u) user=$OPTARG;;
+		p) pass=$OPTARG;;
+		a) echo "Got the a flag - $OPTARG";;
+		b) echo "Got the b flag";;
+		*) echo "ehm.. I don't know $OPTARG";;
+	esac
+done
+
+echo "user: $user / password: $pass"
+
+echo "\$0 is the script itself: $0"
+echo "There are $# arguments given:"
+
+declare -i c=1
+for i in $@
+do
+	echo "$c: $i"
+	((c++))
+done
+
