90d50e30fb1c6542533e1974ef3f557a9e8401ea
Dick de Goede <djdegoed@gmail.com>
2021-05-15 17:50:46 +0200

    added files

--- input.sh
+++ input.sh
@@ -6,7 +6,8 @@ read -p "What is your favorite animal? " animal
 
 echo "name: $name, pass: $pass, animal: $animal"
 
-echo -e "\nWhich animal"
+#echo -e "\nWhich animal"
+PS3="Which animal: "
 select animal in "cat" "dog" "quit"
 do
 	case $animal in
@@ -20,11 +21,46 @@ done
 read -ep "Favourite colour? " -i "Blue" favcolour
 echo $favcolour
 
+# force answer
 read -ep "Favourite food? " favfood
 while [[ -z $favfood ]]; do
-	favfood="bread"
-	# read -ep "Favourite food? (I need an answer) " favfood
+	read -ep "Favourite food? " favfood
 done
 
 echo "Favourite food chosen: $favfood."
 
+# if not supplied provide default answer
+read -ep "Favourite movie? [Shutter Island]? " favmovie
+while [[ -z $favmovie ]]; do
+	# if not specified give a default answer
+	favmovie="Shutter Island"
+done
+
+echo "Favourite movie chosen: $favmovie."
+
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+read -p "What year? [nnnn] " year
+	until [[ $year =~ [0-9]{4} ]]; do
+		read -p "What year [nnnn] " year
+	done
+	echo "Selected year: $year"
